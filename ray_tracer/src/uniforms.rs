4e09123cd25b9f6add947b4598deb91ced7d49b9
jasperdewinther <jasper.dewinther@student.hu.nl>
2021-03-07 01:16:14 +0100

    worked on ray direction

--- ray_tracer/src/uniforms.rs
+++ ray_tracer/src/uniforms.rs
@@ -11,7 +11,7 @@ impl Uniforms {
         Self {
             position: [0.0, 0.0, 0.0],
             time: 0.0,
-            direction: [0.0, 0.0, 1.0],
+            direction: [0.0, 0.0, -1.0],
         }
     }
 
