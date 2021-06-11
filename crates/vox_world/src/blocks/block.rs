f0417052f18bf37111cedbd0e7ce68d5c6a0c5ad
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2021-06-11 19:58:39 +0200

    update

--- crates/vox_world/src/blocks/block.rs
+++ crates/vox_world/src/blocks/block.rs
@@ -11,39 +11,26 @@ pub type BlockId = u8;
 #[inline]
 pub fn get_blocktype(block_id: BlockId) -> BlockType {
     match block_id {
-        0 => BlockType::Grass,
+        0 => BlockType::Air,
         1 => BlockType::Water,
         2 => BlockType::Dirt,
         3 => BlockType::Stone,
         4 => BlockType::Sand,
-        5 => BlockType::Air,
-        6 => BlockType::Leaf,
+        5 => BlockType::Leaf,
+        6 => BlockType::Grass,
         _ => BlockType::Unknown,
     }
 }
 #[inline]
 pub fn get_blockid(block: BlockType) -> BlockId {
     match block {
-        BlockType::Grass => 0,
+        BlockType::Air => 0,
         BlockType::Water => 1,
         BlockType::Dirt => 2,
         BlockType::Stone => 3,
         BlockType::Sand => 4,
-        BlockType::Air => 5,
-        BlockType::Leaf => 6,
+        BlockType::Leaf => 5,
+        BlockType::Grass => 6,
         BlockType::Unknown => 255,
     }
-}
-#[inline]
-pub fn should_render_against(source_block_id: BlockId, neighbor_block_id: BlockId) -> bool {
-    if source_block_id == neighbor_block_id {
-        return false;
-    }
-    if source_block_id == get_blockid(BlockType::Leaf) {
-        return true;
-    }
-    if COLORS[neighbor_block_id as usize][3] == 255.0 {
-        return false;
-    }
-    return true;
 }
